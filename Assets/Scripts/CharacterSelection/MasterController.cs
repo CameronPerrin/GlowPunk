3310d9b9ab18dbe7ed7ca6d4d601773513430621
Zechariah McCarthy <zmccarthy@ltu.edu>
2019-03-21 21:17:25 -0400

    Character selection screen rework
    did work character selection screen added players 3 and 4 to the selection

---
+++ Assets/Scripts/CharacterSelection/MasterController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MasterController : MonoBehaviour
+{
+    public bool P1Ready, P2Ready, P3Ready, P4Ready;
+    public int P1pick, P2pick, P3pick, P4pick;
+    private int Players;//how many players are playing this game
+    public Vector3 P1Spawn, P2Spawn, P3Spawn, P4Spawn;
+
+    public GameObject BeginGameText;
+
+    public List<GameObject> P1Choice, P2Choice, P3Choice, P4Choice;//lists to store all the prefabs
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        P1Ready = true;//This is set to true because a player is ready when they either pick a character or havent began selecting (Currently haven't began selecting)
+        P2Ready = true;
+        P3Ready = true;
+        P4Ready = true;
+
+        P1pick = -1;
+        P2pick = -1;
+        P3pick = -1;
+        P4pick = -1;
+
+        Players = 0;
+
+        /*
+        P1Spawn.position = new Vector3(-35, 0, 0);
+        P1Spawn.rotation = new Quaternion(0,0,0,0);
+        P2Spawn.position = new Vector3(35, 0, 0);
+        P2Spawn.rotation = new Quaternion(0, 0, 0, 0);
+        */
+        //^^Variable initialization^^
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(SceneManager.GetActiveScene().name != "CharacterSelection")
+        {
+            Debug.Log(SceneManager.GetActiveScene().name);
+
+            if (P1pick >= 0 && P1pick < P1Choice.Count)//player 1 picks the assualt character
+            {
+                Instantiate(P1Choice[P1pick], P1Spawn, Quaternion.identity);
+            }
+
+            if (P2pick >= 0 && P2pick < P2Choice.Count)//player 1 picks the assualt character
+            {
+                Instantiate(P2Choice[P2pick], P2Spawn, Quaternion.identity);
+            }
+
+            if (P3pick >= 0 && P3pick < P3Choice.Count)//player 1 picks the assualt character
+            {
+                Instantiate(P3Choice[P3pick], P3Spawn, Quaternion.identity);
+            }
+
+            if (P4pick >= 0 && P4pick < P4Choice.Count)//player 1 picks the assualt character
+            {
+                Instantiate(P4Choice[P4pick], P4Spawn, Quaternion.identity);
+            }
+
+            Destroy(this.gameObject);
+        }
+        if(P1Ready && P2Ready && P3Ready && P4Ready)//players are ready to play
+        {
+            Players = 0;
+            if (P1pick >= 0 && P1pick < P1Choice.Count)//player 1 picks the assualt character
+            {
+                Players++;
+            }
+
+            if (P2pick >= 0 && P2pick < P2Choice.Count)//player 1 picks the assualt character
+            {
+                Players++;
+            }
+
+            if (P3pick >= 0 && P3pick < P3Choice.Count)//player 1 picks the assualt character
+            {
+                Players++;
+            }
+
+            if (P4pick >= 0 && P4pick < P4Choice.Count)//player 1 picks the assualt character
+            {
+                Players++;
+            }
+
+            if (Players > 1)//need at least 2 players to start the game
+            {
+                BeginGameText.SetActive(true);
+                if(Input.GetButtonDown("P1_Pause") || Input.GetButtonDown("P2_Pause") || Input.GetButtonDown("P3_Pause") || Input.GetButtonDown("P4_Pause"))//Start the game
+                {
+                    LoadScene();
+                }
+            }
+            else
+            {
+                BeginGameText.SetActive(false);
+            }
+        }
+    }
+
+    void LoadScene()
+    {
+        DontDestroyOnLoad(this.gameObject);//do not destroy this game object when we go to the next scene
+
+        SceneManager.LoadScene("SampleScene");//testing change later
+
+        
+    }
+}
---
+++ Assets/Scripts/CharacterSelection/P2CharSelect.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2CharSelect : MonoBehaviour
+{
+    public GameObject PressAText;//initial text
+    public List<GameObject> P2ChoiceImage;
+    public List<GameObject> P2ChoiceText;
+    public GameObject ReadyText;
+    public GameObject MC;//this is the master controller that will ultimately spawn the characters in the next scene
+
+    private MasterController MCScript;//grabs the master controllers script so we can manipulate some of its values
+
+    public bool Selecting, IsReady;
+    private int pick;
+    private float buffer, bufferTime;
+
+    private bool isReady = false;//needed for ready up
+    // Start is called before the first frame update
+    void Start()
+    {
+        Selecting = false;
+        IsReady = false;
+        pick = 0;
+        bufferTime = 1;//one second of buffer
+        MCScript = MC.GetComponent<MasterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!Selecting)
+        {
+            if (Input.GetButtonDown("A2"))//first player presses A
+            {
+                Selecting = true;
+                PressAText.SetActive(false);
+                MCScript.P2Ready = false;//selecting character therefore cannot be ready to start
+
+                P2ChoiceImage[pick].SetActive(true);
+                P2ChoiceText[pick].SetActive(true);
+            }
+        }
+        else if (!IsReady)//Selecting Character but not ready
+        {
+            if (Input.GetButtonDown("A2"))//selects a character
+            {
+                IsReady = true;
+                MCScript.P2Ready = true;//player chose a character therefore is ready to play
+
+                for (int i = 0; i < P2ChoiceImage.Count; i++)//set character selections as false
+                {
+                    P2ChoiceImage[i].SetActive(false);
+                    P2ChoiceText[i].SetActive(false);
+                }
+                //^^Set all our UI stuff false for Ready up
+
+                ReadyText.SetActive(true);//Show that the player is ready
+                MCScript.P2pick = pick;//tell master controller which character player 2 selected
+                MCScript.P2Ready = true;
+            }
+            else if (Input.GetButtonDown("B2"))//player backs out
+            {
+                Selecting = false;
+                MCScript.P2Ready = true;//player backed out and is not playing therefore ready up so other players can start
+                for (int i = 0; i < P2ChoiceImage.Count; i++)//set character selections as false
+                {
+                    P2ChoiceImage[i].SetActive(false);
+                    P2ChoiceText[i].SetActive(false);
+                }
+                //^^Set all our UI stuff false since player backed out
+                PressAText.SetActive(true);
+            }
+            else if (Input.GetAxis("MoveJoy2X") != 0 && buffer <= 0)
+            {
+                for (int i = 0; i < P2ChoiceImage.Count; i++)//set character selections as false
+                {
+                    P2ChoiceImage[i].SetActive(false);
+                    P2ChoiceText[i].SetActive(false);
+                }
+
+                if (Input.GetAxis("MoveJoy2X") < 0)//input is left
+                {
+                    if (pick == 0)//loop to end of array
+                    {
+                        pick = P2ChoiceImage.Count - 1;//set to the end of the array
+                    }
+                    else
+                    {
+                        pick--;
+                    }
+                }
+                else//input is right
+                {
+                    if (pick == P2ChoiceImage.Count - 1)//at the end of the array loop back to beginning 
+                    {
+                        pick = 0;
+                    }
+                    else
+                    {
+                        pick++;
+                    }
+                }
+                P2ChoiceImage[pick].SetActive(true);
+                P2ChoiceText[pick].SetActive(true);
+
+                buffer = bufferTime;
+            }
+            if (buffer > 0)
+            {
+                buffer -= Time.deltaTime;
+            }
+        }
+        else//Player is currently readied up
+        {
+            if (Input.GetButtonDown("B2"))//if player decides to not play or change character
+            {
+                IsReady = false;
+                ReadyText.SetActive(false);
+                MCScript.P2pick = -1;//-1 means no character is selected
+                MCScript.P2Ready = false;//player is no longer ready
+
+                P2ChoiceImage[pick].SetActive(true);
+                P2ChoiceText[pick].SetActive(true);
+            }
+        }
+    }
+}
