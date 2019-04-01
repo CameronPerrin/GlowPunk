e6272f72a023cc2c0a26f00c533c9641f30656fd
Zechariah McCarthy <zmccarthy@ltu.edu>
2019-03-31 22:02:05 -0400

    Main Menu functionality
    Added controller functionality to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,67 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public List<GameObject> Images;
+    private int selected;
+    private float buffer, bufferTime;
+
+    void Start() {
+        selected = 0;
+        Images[0].SetActive(true);
+        bufferTime = .2f;
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("MoveJoy1Y") > 0 && buffer <= 0)//user presses upward
+        {
+            if(selected + 1 == Images.Count)//stay in the bounds of our options
+            {
+                selected = 0;
+            }
+            else
+            {
+                selected++;
+            }
+            buffer = bufferTime;
+        }
+        else if(Input.GetAxis("MoveJoy1Y") < 0 && buffer <= 0)//user inputs downwards
+        {
+            if(selected == 0)//stay in the bounds of our options
+            {
+                selected = Images.Count - 1;
+            }
+            else
+            {
+                selected--;
+            }
+            buffer = bufferTime;
+        }
+        
+        for(int i = 0; i < Images.Count; i++)
+        {
+            Images[i].SetActive(false);
+        }
+        Images[selected].SetActive(true);
+        //^^Show the current option we are hovering^^
+
+        if(Input.GetButtonDown("A1"))//player selects an option
+        {
+            if(selected == 0)//play the game
+            {
+                PlayGame();
+            }
+            else//quit out
+            {
+                QuitGame();
+            }
+        }
+
+        if(buffer > 0)
+        {
+            buffer -= Time.deltaTime;
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("CharacterSelection");
