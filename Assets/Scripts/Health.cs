993e5b2597e39a107981ea9f6cd4ded08144c291
Zechariah McCarthy <zmccarthy@ltu.edu>
2019-04-08 20:59:11 -0400

    Damage text bug fix
    Made damage numbers independent of players so now they do not flip

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -165,22 +165,22 @@ public class Health : MonoBehaviour
             {
                 if (currentPlayer == 1 && (collision.tag == "User2" || collision.tag == "User3" || collision.tag == "User4"))
                 {
-                    ShowFloatingText_SMG(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_SMG(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti); // needed to use this for healthbars
                 }
                 else if (currentPlayer == 2 && (collision.tag == "User1" || collision.tag == "User3" || collision.tag == "User4"))
                 {
-                    ShowFloatingText_SMG(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_SMG(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti);
                 }
                 else if (currentPlayer == 3 && (collision.tag == "User1" || collision.tag == "User2" || collision.tag == "User4"))
                 {
-                    ShowFloatingText_SMG(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_SMG(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti);
                 }
                 else if (currentPlayer == 4 && (collision.tag == "User1" || collision.tag == "User2" || collision.tag == "User3"))
                 {
-                    ShowFloatingText_SMG(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_SMG(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti);
                 }
             }
@@ -188,22 +188,22 @@ public class Health : MonoBehaviour
             {
                 if (currentPlayer == 1 && (collision.tag == "User2" || collision.tag == "User3" || collision.tag == "User4"))
                 {
-                    ShowFloatingText_Normal(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_Normal(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti); // needed to use this for healthbars
                 }
                 else if (currentPlayer == 2 && (collision.tag == "User1" || collision.tag == "User3" || collision.tag == "User4"))
                 {
-                    ShowFloatingText_Normal(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_Normal(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti);
                 }
                 else if (currentPlayer == 3 && (collision.tag == "User1" || collision.tag == "User2" || collision.tag == "User4"))
                 {
-                    ShowFloatingText_Normal(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_Normal(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti);
                 }
                 else if (currentPlayer == 4 && (collision.tag == "User1" || collision.tag == "User2" || collision.tag == "User3"))
                 {
-                    ShowFloatingText_Normal(bulletDamage * share.damageMulti);
+                    //ShowFloatingText_Normal(bulletDamage * share.damageMulti);
                     modifyHealth(bulletDamage * share.damageMulti);
                 }
             }
@@ -212,22 +212,22 @@ public class Health : MonoBehaviour
         {
             if (currentPlayer == 1 && (collision.tag == "User2" || collision.tag == "User3" || collision.tag == "User4"))
             {
-                ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
+                //ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
                 modifyHealth(sniperDamage); // needed to use this for healthbars
             }
             else if (currentPlayer == 2 && (collision.tag == "User1" || collision.tag == "User3" || collision.tag == "User4"))
             {
-                ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
+                //ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
                 modifyHealth(sniperDamage);
             }
             else if (currentPlayer == 3 && (collision.tag == "User1" || collision.tag == "User2" || collision.tag == "User4"))
             {
-                ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
+                //ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
                 modifyHealth(sniperDamage);
             }
             else if (currentPlayer == 4 && (collision.tag == "User1" || collision.tag == "User2" || collision.tag == "User3"))
             {
-                ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
+                //ShowFloatingText_Sniper(sniperDamage * share.damageMulti);
                 modifyHealth(sniperDamage);
             }
         }
